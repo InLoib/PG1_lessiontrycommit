41381e2c6573bbd6740a9d9af7874e761471d9b4
Ina Loibner <ina.loibner@edu.campus02.at>
2016-10-29 14:41:00 +0200

    try to commit lession-java

---
+++ src/Übung1_a_Typen.java
@@ -0,0 +1,20 @@
+
+public class Übung1_a_Typen {
+
+	public static void main(String[] args) {
+	
+	int ganzzahl = 42;
+	boolean isSunny = true;
+	float piApprox = 3.14f;
+	double euler = 2.714387869552;
+	char letterA= 'a';
+			
+	System.out.println(ganzzahl);
+	System.out.println(3.14);
+	System.out.println(isSunny);
+	System.out.println(piApprox);
+	System.out.println(euler);
+	System.out.println(letterA);
+	
+}
+}
